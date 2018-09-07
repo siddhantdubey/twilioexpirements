3d4e3a90633fda41ec17b77f13f85adbc699391f
Siddhant Dubey <sid12.dubey@gmail.com>
2018-09-07 07:21:40 -0400

    Nothing changed, just some spaces to tabs

--- textMyself.py
+++ textMyself.py
@@ -3,10 +3,10 @@ import time
 from twilio.rest import Client
 
 
-account_sid = ""
-auth_token = ""
+account_sid = "AC6539b20aac8f122fccd2654f98c57fad"
+auth_token = "9b568826df59a8b04aaae9fb307c0c17"
 
-twilioNumber = ''
+twilioNumber = '+1 910 415 1870'
 
 client = Client(account_sid, auth_token)
 
@@ -23,43 +23,18 @@ def textmyself():
 
 def textdaily(message):
 	client.api.account.messages.create(
-	    to='',
+	    to='+1 848 234 5354',
 	    from_=twilioNumber,
     	    body=message)
 
 
-schedule.every().day.at("04:00").do(textdaily, "You got this bro")
-
-schedule.every().day.at("07:20").do(textdaily, "Ayyy they gonna watch your CSPAN today")
-
-schedule.every().day.at("08:20").do(textdaily, "Do dat chemistry")
-
-schedule.every().day.at("09:13").do(textdaily, "Oof advisory time")
-
-schedule.every().day.at("09:35").do(textdaily, "Get your science on you ready for the test?")
-
-schedule.every().day.at("10:28").do(textdaily, "Stuff your face time")
-
-schedule.every().day.at("11:03").do(textdaily, "Have fun with your socrative seminar")
-
-schedule.every().day.at("11:51").do(textdaily, "Oh la la!!! Tu vas au salle de francais")
-
-schedule.every().day.at("12:44").do(textdaily, "Have fun doing nothing in resource")
-
-schedule.every().day.at("13:37").do(textdaily, "Have fun in tech dr/design")
-
-schedule.every().day.at("14:30").do(textdaily, "School is over, now you gots homework to do")
-
-schedule.every().day.at("16:00").do(textdaily, "Start studying math")
-
-schedule.every().day.at("17:30").do(textdaily, "Chemistry")
-
-schedule.every().day.at("18:15").do(textdaily, "Physics")
-
-schedule.every().day.at("19:00").do(textdaily, "SAT PREP")
-
-schedule.every().day.at("19:45").do(textdaily, "You are free to go soldier!")
-
+schedule.every().day.at("07:30").do(textdaily, "Yeet 1")
+schedule.every().day.at("08:30").do(textdaily, "Yeet 2")
+schedule.every().day.at("10:30").do(textdaily, "Yeet 3")
+schedule.every().day.at("11:30").do(textdaily, "Yeet 4")
+schedule.every().day.at("12:30").do(textdaily, "Yeet 5")
+schedule.every().day.at("13:30").do(textdaily, "Yeet 6")
+schedule.every().day.at("14:30").do(textdaily, "Yeet 7")
 
 while True:
     schedule.run_pending()
